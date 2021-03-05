d3e082d5f13331e1c164af2fc9da38ae9a642026
Kacyn1 <62652276+Kacyn1@users.noreply.github.com>
2021-03-05 12:26:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,55 +9,68 @@ using namespace std;
 class liczby_polpierwsze {
 	ifstream plik;
 	
-	public:
-		liczby_polpierwsze();
-		~liczby_polpierwsze();
-		void wczytaj();
-		bool sprawdzenie(int);
+public:
+	liczby_pierwsze();
+	~liczby_pierwsze();
+	void wczyt();
+	bool sprawdzenie(int);
+	
 };
-liczby_polpierwsze::liczby_polpierwsze() {
-	plik.open("ciagi.txt");
+liczby_pierwsze::liczby_pierwsze() {
+plik.open("ciagi.txt");
 }
-void liczby_polpierwsze::wczytaj() {
-	vector <int> polpierwsze;
+
+void liczby_pierwsze::wczyt() {
+	
+	vector <int> pierwsze;
 	string ciag;
 	int dec;
 	if(plik.good()) {
-		while(!plik.eof()) {
-			plik>>ciag;
-			bitset<32> bin(ciag);
-			dec = bin.to_ulong();
-			if(this->sprawdzenie(dec)) {
-				polpierwsze.push_back(dec);
-			}
-		}		
-	}
-	cout<<polpierwsze.size()<<"\n";
-	cout<<"min: "<< *min_element(polpierwsze.begin(), polpierwsze.end())<<"\n"; 
-	cout<<"max: "<< *max_element(polpierwsze.begin(), polpierwsze.end()); 
-}
-bool liczby_polpierwsze::sprawdzenie(int ciag) {
-	vector <int> dzielniki;
-	for(int i=2; i<=ciag/2; i++) {
-		int c=ciag;
-		while(c%i==0){
-			c=c/i;
-			dzielniki.push_back(c);
+		
+	while(!plik.eof()) {
+		
+		plik>>ciag;
+		bitset<32> bin(ciag);
+		dec = bin.to_ulong();
+		if(this->sprawdzenie(dec)) {
+			pierwsze.push_back(dec);
 		}
+		
+	}	
+		
+}
+	
+cout<<pierwsze.size()<<"\n";
+cout<<"min: "<< *min_element(pierwsze.begin(), pierwsze.end())<<"\n"; 
+cout<<"max: "<< *max_element(pierwsze.begin(), pierwsze.end()); 
+}
+
+bool liczby_pierwsze::sprawdzenie(int ciag) {
+vector <int> dzielniki;
+for(int i=2; i<=ciag/2; i++) {
+	int c=ciag;
+	while(c%i==0){
+		c=c/i;
+		dzielniki.push_back(c);
 	}
+}
+	
 	if(dzielniki.size()==2) {
-		return true;
+	return true;
 	}
 	else {
-		return false;
+	return false;
 	}
 }
-liczby_polpierwsze::~liczby_polpierwsze() {
+
+liczby_pierwsze::~liczby_pierwsze() {
 	plik.close();
 }
 
+
+
 int main(int argc, char** argv) {
-	liczby_polpierwsze c;
-	c.wczytaj();
-	return 0;
+liczby_pierwsze c;
+c.wczyt();
+return 0;
 }
